73068fac1877e3e78835a197d23275ae05e8ed90
Kamil Makoś <makos.kamil@o2.pl>
2018-07-15 22:13:10 +0200

    change_contact

--- components/Contact.js
+++ components/Contact.js
@@ -4,15 +4,12 @@ var Contact = React.createClass ({
     },
     render: function () {
         return (
-            React.createElement('div', {className: 'contactItem'},
-                React.createElement('img', {
-                    className: 'contactImage',
-                    src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
-            }),
-            React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-            React.createElement('p', {className: 'contactLabel'}, 'Nazwisko ' + this.props.item.LastName),
-            React.createElement('a', {className: 'contactEmail'}, 'mailto: ' + this.props.item.Email),
-                this.props.item.email
+            <div className={'contactItem'}>
+                <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
+                <p className={'contactLabel'}>Imię: {this.props.contact.firstName}</p>
+                <p className={'contactLabel'}>Nazwisko: {this.props.contact.firstName}</p>
+                <a href={'mailto:' + this.props.item.email}>{this.props.item.email}</a>
+            </div>
             )
         )
     }
